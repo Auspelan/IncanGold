0a87c536447ffeedcdc9a071093965b3566e0061
yuxunhe <ader05817@gmail.com>
2025-11-04 21:28:53 +0800

    fix(chaincode): set default enteryFee to 0.01 ether

--- chaincode/migrations/2_deploy_contracts.js
+++ chaincode/migrations/2_deploy_contracts.js
@@ -5,6 +5,6 @@ const IncanGold = artifacts.require("IncanGold");
 module.exports = function (deployer) {
   // Let's set the entry fee to 0.1 Ether for testing.
   // 1 Ether = 1e18 wei, so 0.1 Ether = 1e17 wei.
-  const entryFee = web3.utils.toWei("0.1", "ether");
+  const entryFee = web3.utils.toWei("0.01", "ether");
   deployer.deploy(IncanGold, entryFee);
 };
